13f239482734c9d079cbebd229d3909cb3f66301
Toni Haryanto <toha.samba@gmail.com>
2012-10-22 17:11:40 +0700

    initial commit for language files

---
+++ language/english/chequest_lang.php
@@ -0,0 +1,15 @@
+<?php
+	
+// General Titles
+$lang['chequest:title']				= 'cheQuest';
+$lang['chequest:title:general']		= 'General';
+$lang['chequest:title:details']		= 'Your Details';
+$lang['chequest:title:address']		= 'Your Address';
+$lang['chequest:title:bill']		= 'Billing Details';
+$lang['chequest:title:ship']		= 'Shipping Details';
+
+// Sections
+$lang['chequest:sections:dashboard'] 	= 'Dashboard';
+$lang['chequest:sections:activity'] 	= 'Activity';
+$lang['chequest:sections:friend'] 		= 'Friend';
+$lang['chequest:sections:settings']		= 'Settings';
