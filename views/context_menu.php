8b3525d80a64c5e5cac79b446a3747e151d513da
toni haryanto <toha.samba@gmail.com>
2012-12-08 08:15:17 +0700

    change line endings to linux based (LF)

--- views/context_menu.php
+++ views/context_menu.php
@@ -1,1 +1,12 @@
-<ul class="nav nav-tabs">	<li<?php echo ('profile' == $context)? ' class="active"': ''; ?>>		<a href="<?php echo site_url('chequest/profile'); ?>" title="<?php echo $this->current_user->username; ?>">			<span class="main-avatar">				<?php echo isset($this->current_user->avatar)? "<img src='".$this->current_user->avatar."' />" : ''; ?>			</span>		</a>	</li>	<?php foreach($navs as $nav): ?>		<li<?php echo ($nav->context_slug == $context)? ' class="active"': ''; ?>><a href="<?php echo site_url('chequest/'.$nav->context_slug); ?>"><?php echo (lang('chequest:context:'.$nav->context_slug))? lang('chequest:context:'.$nav->context_slug) : $nav->context_slug; ?></a></li>	<?php endforeach; ?></ul>
+<ul class="nav nav-tabs">
+	<li<?php echo ('profile' == $context)? ' class="active"': ''; ?>>
+		<a href="<?php echo site_url('chequest/profile'); ?>" title="<?php echo $this->current_user->username; ?>">
+			<span class="main-avatar">
+				<?php echo isset($this->current_user->avatar)? "<img src='".$this->current_user->avatar."' />" : ''; ?>
+			</span>
+		</a>
+	</li>
+	<?php foreach($navs as $nav): ?>
+		<li<?php echo ($nav->context_slug == $context)? ' class="active"': ''; ?>><a href="<?php echo site_url('chequest/'.$nav->context_slug); ?>"><?php echo (lang('chequest:context:'.$nav->context_slug))? lang('chequest:context:'.$nav->context_slug) : $nav->context_slug; ?></a></li>
+	<?php endforeach; ?>
+</ul>
