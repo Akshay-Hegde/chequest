0f7b012738849f6d3c401779b747403c3f26c12e
toni haryanto <toha.samba@gmail.com>
2012-12-12 18:37:08 +0700

    modify language for discussion and some other features

--- language/english/chequest_lang.php
+++ language/english/chequest_lang.php
@@ -15,7 +15,10 @@
 	$lang['chequest:sections:settings']		= 'Settings';
 	$lang['chequest:sections:addresses'] 	= 'Addresses';
 	$lang['chequest:sections:orders_items']	= 'Order Items';
-	$lang['chequest:sections:gateways']		= 'Gateways';
+	$lang['chequest:sections:discussion_group']		= 'Discussion Group';
+	$lang['chequest:sections:discussion_topic']		= 'Discussion Topic';
+	$lang['chequest:sections:discussion_thread']	= 'Discussion Thread';
+	$lang['chequest:sections:discussion_moderator']	= 'Discussion Moderator';
 	$lang['chequest:sections:settings'] 	= 'Settings';
 	
 	// Shortcuts
@@ -43,140 +46,8 @@
 	$lang['chequest:context:profile']		= 'Profile';
 	$lang['chequest:context:settings']		= 'Settings';
 
+
+
 	// Sub Context
 	$lang['chequest:context:all_activity']		= 'All Activity';
 	
-	// Products
-	$lang['chequest:prod_none']				= 'No Products Found';
-	$lang['chequest:prod_create'] 			= 'Create Product';
-	$lang['chequest:prod_header']			= 'Edit %t';
-	$lang['chequest:prod_title']			= 'Manage Products';
-	$lang['chequest:prod_title_create'] 	= 'Create New Product';
-	$lang['chequest:prod_title_edit'] 		= 'Edit Product';
-	$lang['chequest:prod_edit_success'] 	= 'Product edited successfully';
-	$lang['chequest:prod_edit_error'] 	 	= 'Product edit failed';
-	$lang['chequest:prod_add_success'] 		= 'A new product was added successfully';
-	$lang['chequest:prod_add_error'] 		= 'There was a problem adding a new product';
-	$lang['chequest:prod_delete_error'] 	= 'There was a problem deleting that product';
-	$lang['chequest:prod_delete_success'] 	= 'Product deleted successfully';
-	$lang['chequest:prod_duplicate_error'] 	= 'There was a problem duplicating that product';
-	$lang['chequest:prod_duplicate_success']= 'Product duplicated successfully';
-	$lang['chequest:prod_not_found'] 		= 'That product cannot be found';
-
-	// Labels
-	$lang['chequest:label_draft']		= 'Draft';
-	$lang['chequest:label_live'] 		= 'Live';
-	$lang['chequest:label_id'] 			= 'Product Code';
-	$lang['chequest:label_title'] 		= 'Title';
-	$lang['chequest:label_slug'] 		= 'Slug';
-	$lang['chequest:label_status'] 		= 'Status';
-	$lang['chequest:label_description']	= 'Description';
-	$lang['chequest:label_category']	= 'Category';
-	$lang['chequest:label_parent']		= 'Parent Category';
-	$lang['chequest:label_filtercat']	= 'Filter by Category';
-	$lang['chequest:label_filtersel']	= 'Select a Category';
-	$lang['chequest:label_rrp']			= 'Recommended Retail Price';
-	$lang['chequest:label_rrp_tax']		= 'Recommended Retail Price (before tax)';
-	$lang['chequest:label_rrp_short']	= 'RRP';
-	$lang['chequest:label_price']		= 'Current Price';
-	$lang['chequest:label_price_tax']	= 'Current Price (before tax)';
-	$lang['chequest:label_stock']		= 'Current Stock Level';
-	$lang['chequest:label_drop_images'] = 'Drop images here to upload';
-	$lang['chequest:label_duplicate']   = 'Duplicate';
-
-	$lang['chequest:label_stock_short']	= 'Stock Level';
-	$lang['chequest:label_stock_status']= 'Stock Status';
-	$lang['chequest:label_stock_in']	= 'In Stock';
-	$lang['chequest:label_stock_low']	= 'Low Stock';
-	$lang['chequest:label_stock_out']	= 'Out of Stock';
-	$lang['chequest:label_stock_order']	= 'More stock ordered';
-	$lang['chequest:label_stock_ended']	= 'Discontinued';
-
-	$lang['chequest:label_remove']		= 'Remove';
-	$lang['chequest:label_image']		= 'Image';
-	$lang['chequest:label_images']		= 'Images';
-	$lang['chequest:label_order']		= 'Order';
-	$lang['chequest:label_gateway']		= 'Payment Method';
-	$lang['chequest:label_shipping']	= 'Shipping Method';
-	$lang['chequest:label_quantity']	= 'Quantity';
-	$lang['chequest:label_price_total'] = 'Total Price';
-	$lang['chequest:label_price_ship']	= 'Shipping Cost';
-	$lang['chequest:label_price_sub']	= 'Sub-total';
-	$lang['chequest:label_ship_to']		= 'Shipped to';
-	$lang['chequest:label_bill_to']		= 'Billed to';
-	$lang['chequest:label_date']		= 'Date';
-	$lang['chequest:label_product']		= 'Product';
-	$lang['chequest:label_products']	= 'Products';
-	$lang['chequest:label_company']		= 'Company Name';
-	$lang['chequest:label_firstname']	= 'First Name';
-	$lang['chequest:label_lastname']	= 'Last Name';
-	$lang['chequest:label_phone']		= 'Phone';
-	$lang['chequest:label_email']		= 'Email Address';
-	$lang['chequest:label_address1']	= 'Address Line 1';
-	$lang['chequest:label_address2']	= 'Address Line 2';
-	$lang['chequest:label_city']		= 'City';
-	$lang['chequest:label_postcode']	= 'Postcode';
-	$lang['chequest:label_county']		= 'County';
-	$lang['chequest:label_country']		= 'Country';
-	$lang['chequest:label_details'] 	= 'My Billing and Shipping addresses are the same';
-	$lang['chequest:label_user_order']	= 'User';
-	$lang['chequest:label_ip']			= 'IP Address';
-
-	$lang['chequest:label_nameaz']		= 'Name A - Z';
-	$lang['chequest:label_nameza']		= 'Name Z - A';
-	$lang['chequest:label_pricelow']	= 'Price Low &gt; High';
-	$lang['chequest:label_pricehigh']	= 'Pirce High &gt; Low';
-	$lang['chequest:label_modelaz']		= 'Model A - Z';
-	$lang['chequest:label_modelza']		= 'Model Z - A';
-
-	$lang['chequest:label_time_now']	= 'less than a minute ago.';
-	$lang['chequest:label_time_min']	= 'about a minute ago.';
-	$lang['chequest:label_time_mins']	= 'about %s minutes ago.';
-	$lang['chequest:label_time_hour']	= 'about an hour ago.';
-	$lang['chequest:label_time_hours']	= 'about %s hours ago.';
-	$lang['chequest:label_time_day']	= '1 day ago.';
-	$lang['chequest:label_time_days'] 	= '%s days ago.';
-
-	// Orders
-	$lang['chequest:orders:title']				= 'Orders';
-	$lang['chequest:orders:no_orders']			= 'There are currently no orders';
-	$lang['chequest:orders:my_orders']			= 'My Orders';
-	$lang['chequest:orders:view_order']			= 'View Order #%s';
-	$lang['chequest:orders:title_create'] 		= 'Create Order';
-	$lang['chequest:orders:title_edit']	  		= 'Edit Order #%s';
-	$lang['chequest:orders:delete_success'] 	= 'Order deleted successfully';
-	$lang['chequest:orders:delete_error']		= 'Order was not deleted due to an issue';
-	$lang['chequest:orders:save_first']			= 'Please save the order before adding products';
-	$lang['chequest:orders:delete']				= 'Delete Orders';
-	$lang['chequest:orders:mark_as']			= 'Mark as ';
-	$lang['chequest:orders:status_unpaid'] 		= 'Unpaid';
-	$lang['chequest:orders:status_paid'] 		= 'Paid';
-	$lang['chequest:orders:status_dispatched']	= 'Dispatched';
-	$lang['chequest:orders:status_processing']	= 'Processing';
-	$lang['chequest:orders:status_refunded']	= 'Refunded';
-	$lang['chequest:orders:status_cancelled']	= 'Cancelled';
-	$lang['chequest:orders:logged_in']			= 'You must be logged in to view your order history.';
-
-	// Gateways
-	$lang['chequest:gateways:admin_title']				= 'Payment Gateways';
-	$lang['chequest:gateways:install_title']			= 'Install a Gateway';
-	$lang['chequest:gateways:installed_title']			= 'Installed Gateways';
-	$lang['chequest:gateways:no_gateways']				= 'There are currently no payment gateways installed.';
-	$lang['chequest:gateways:no_uninstalled_gateways']	= 'All available gateways are currently installed.';
-	$lang['chequest:gateways:errors:invalid_bool']		= 'The %s field must be a boolean value.';
-	$lang['chequest:gateways:warning'] 					= 'All gateway settings will be lost and your store may be unable to take payments! Are you sure you want to uninstall this gateway?';
-	
-	// Checkout
-	$lang['chequest:gateways:labels:name']			= 'Name';
-	$lang['chequest:gateways:labels:desc']			= 'Description';
-	$lang['chequest:cart:title']					= 'Shopping Cart';
-	$lang['chequest:cart:empty']					= 'There are currently no items in your cart';
-	$lang['chequest:checkout:title'] 				= 'Checkout';
-	$lang['chequest:checkout:error_callback'] 		= 'There seems to have been a problem with your order, please try again, possibly using another payment method.';
-	$lang['chequest:payment:title'] 				= 'Confirm Details';
-	$lang['chequest:payment:title_success'] 		= 'Payment Complete';
-	$lang['chequest:checkout:title:ship_method']	= 'Shipping Method';
-	$lang['chequest:checkout:title:payment_method']	= 'Payment Method';
-
-	// Addresses
-	$lang['chequest:addresses:title'] = 'My Addresses';
